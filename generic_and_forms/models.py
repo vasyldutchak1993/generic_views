9919220b01c6805e36eec1492866cc921a2e501a
vasyldutchak1993 <vasuldutschak@mail.ru>
2025-08-12 17:35:56 +0200

    create model jacket

--- generic_and_forms/models.py
+++ generic_and_forms/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Jacket(models.Model):
+    brand = models.CharField(max_length=100, verbose_name="Značka")
+    color = models.CharField(max_length=50, verbose_name="Farba")
+
+    def __str__(self):
+        return f"{self.brand} ({self.color})"
